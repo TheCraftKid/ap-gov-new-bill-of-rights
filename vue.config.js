d14e9331c2ca751c8107696541fe4d343a82f190
Willie Chalmers III <willieechalmers@gmail.com>
2018-12-17 23:02:28 -0700

    chore: Add support for pre-rendering

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') {
+      return;
+    }
+    config.plugins.push(new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: [
+        '/',
+      ],
+    }));
+  },
+};
